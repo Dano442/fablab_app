605b6f05ba012548a82c750c81348b50082bee20
Dani <danielysraelronceroscornejo@gmail.com>
2025-09-02 01:14:20 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fablab_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/fablab_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fablab_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
