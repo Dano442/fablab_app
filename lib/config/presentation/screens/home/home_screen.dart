39b42f6091031ba7befc71c3b0aa5680bbb02945
Dani <danielysraelronceroscornejo@gmail.com>
2025-09-03 11:38:12 -0400

    home screen implementado

--- lib/config/presentation/screens/home/home_screen.dart
+++ lib/config/presentation/screens/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fablab_app/config/menu/menu_items.dart';
 import 'package:flutter/material.dart';
 
 
@@ -8,9 +9,55 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const  Text('Hola Usuario, bienvenido al Panel Administrativo'),
+        title: const  Text('Bienvenido al Panel Administrativo'),
       ),
+      body: const _HomeView(),
 
     );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  const _HomeView();
+
+  @override
+  Widget build(BuildContext context) {
+
+
+
+
+    return ListView.builder(
+      
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index){
+        final menuItem = appMenuItems[index];
+
+        return _CustomListTile(menuItem: menuItem);
+      }
+      );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+
+    return ListTile(
+      leading: Icon(menuItem.icon, color: colors.primary ),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary,),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      onTap: (){
+        
+      },
+      );
   }
 }
